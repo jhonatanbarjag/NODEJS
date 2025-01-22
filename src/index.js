7c0ae8e16078e7561da5dcb79be150b6a55bc6e6
jhonatanbarjag <jhonatanbarja.g@hotmail.com>
2025-01-21 20:59:23 -0500

    Actualizar proyecto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hola mundo.");
+import express from 'express';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('index'));
+app.listen(3000)
+console.log('Server running at http://localhost:3000/');
